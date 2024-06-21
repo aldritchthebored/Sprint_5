066e7fbe1d82d83cd9821e0841ee6a98c990166b
Elizaveta Vinogradova <lizavin2@gmail.com>
2024-06-22 00:38:41 +0300

    fix for test_registration_new_user

--- tests/test_registration_page.py
+++ tests/test_registration_page.py
@@ -10,8 +10,8 @@ class TestStellarBurgers:
 
         driver.find_element(By.XPATH,'//*[@id="root"]/div/main/div/div/p[1]/a').click()  # переходим на страницу авторизации
         # вводим данные для авторизации с неправильным паролем
-        driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[1]/div/div/input').send_keys('Елизавета')
-        driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[2]/div/div/input').send_keys('elizavetavinogradova10562@yandex.ru')
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[1]/div/div/input').send_keys('Алексей')
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[2]/div/div/input').send_keys('xxxxxxxx@ya.ru')
         driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[3]/div/div/input').send_keys('123456')
          #нажимаем кнопку "Зарегистрироваться"
         driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/button').click()
